738d4707a1d74dd22b8a750ceec509bbe82042b9
Graham <geckoman009@gmail.com>
2017-12-25 23:12:16 -0500

    remove unused variables

--- src/arch/chip8/chip8_cpu.hpp
+++ src/arch/chip8/chip8_cpu.hpp
@@ -21,10 +21,6 @@ private:
 	std::size_t length;
 	std::ifstream file;
 	std::unique_ptr<std::uint8_t[]> buffer;
-	std::uint16_t* cur_instrs;
 	std::vector<std::uint8_t> registers;
-	std::uint16_t ir; /* index register */
-	std::uint16_t pc; /* program counter */
-	std::stack<std::uint16_t> stack;
 	std::unique_ptr<std::uint8_t[]> memory;
 };
